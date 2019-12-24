9a44dc519aab221e0657e135312c58f1cf37aafc
Tao Wang <tao.wang@certegy.com.au>
2019-12-24 14:02:42 +1030

    Create the CelestialObjectController Class

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+    }
+}
