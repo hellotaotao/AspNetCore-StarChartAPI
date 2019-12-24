388a319972c7389231941d7da69141c7c6adf898
Tao Wang <tao.wang@certegy.com.au>
2019-12-24 13:36:32 +1030

    Create CelestialObject Model

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace StarChart.Data
+namespace StarChart.Models
 {
     public class CelestialObject
     {
